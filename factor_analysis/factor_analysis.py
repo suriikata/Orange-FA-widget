24ca9a9c3f60a47e13810fb363e52ceb16b4323f
suriikata <iris.cilensek1@gmail.com>
2022-06-05 20:49:10 +0200

    optimizmus kompleksus

--- factor_analysis/factor_analysis.py
+++ factor_analysis/factor_analysis.py
@@ -46,6 +46,8 @@ class OWFactorAnalysis(OWWidget):
     icon = "icons/DataSamplerB.svg"
     priority = 20
 
+    want_control_area = False
+
     class Inputs:
         data = Input("Data", Table)
 
@@ -53,26 +55,25 @@ class OWFactorAnalysis(OWWidget):
         sample = Output("Sampled Data", Table)
 
     n_components = settings.ContextSetting(1)
-    setting_for_rotation = settings.Setting(Rotation.NoRotation)
+    rotation = settings.Setting(Rotation.NoRotation)
     autocommit = settings.Setting(True)
     
     def __init__(self):
-        super().__init__()  # since OWFactorAnalysis is a derivative of OWWidget, first intialize OWFA
         self.dataset = None
+        self.attributes = None
+        self.fa_loadings = None
 
         # Main area settings
-        self.mainArea.setVisible(True)
-
         self.attr_box = gui.hBox(self.mainArea, margin=0)
         
         gui.spin(
             self.attr_box, self, "n_components", label="Number of components:",
             minv=1, maxv=100, step=1, controlWidth=30, 
-            callback=[self.factor_analysis, self.commit.deferred],  # deferred = zapoznelo
+            callback=self.n_components_changed,
         )
-        
+
         gui.comboBox(
-            self.attr_box, self, "setting_for_rotation", label="Rotation:", labelWidth=50,
+            self.attr_box, self, "rotation", label="Rotation:", labelWidth=50,
             items=Rotation.items(), orientation=Qt.Horizontal, 
             contentsLength=12, callback=self.factor_analysis
         )
@@ -81,7 +82,8 @@ class OWFactorAnalysis(OWWidget):
             self.attr_box, self, 'autocommit', 'Commit',
             orientation=Qt.Horizontal
         )
-        gui.separator(self.mainArea) # do i need it? >>> the first element in mainArea
+
+        gui.separator(self.mainArea)
 
         box = gui.vBox(self.mainArea, box = "Factor Loadings")
         
@@ -100,8 +102,12 @@ class OWFactorAnalysis(OWWidget):
         # view.clicked.connect(self.cell_clicked)
         box.layout().addWidget(view)
 
-        self.plot = SliderGraph("Factor 1", "Factor 2", self.prazna_funkcija)
+        self.plot = SliderGraph("Factor 1", "Factor 2", lambda x: None)
         self.mainArea.layout().addWidget(self.plot)
+
+    def n_components_changed(self):
+        self.factor_analysis()
+        self.commit.deferred()
 
     def insert_item(self, i, j, val):
         """
@@ -127,43 +133,23 @@ class OWFactorAnalysis(OWWidget):
 
     def insert_table(self):
         #insert variable names into first row (0)
-        for j in range(len(self.dataset.domain.attributes)):
-            name = self.dataset.domain.attributes[j].name
+        for j in range(len(self.attributes)):
+            name = self.attributes[j].name
             self.insert_item(0, j + 1, name)        #in order to get the first cell in column > increase j for 1
 
         for i in range(self.n_components):
             name = f"Factor {i + 1}"
             self.insert_item(i + 1, 0, name)        #in order to get the first cell in row > increase i for 1
 
-        for i in range(len(self.result.X)):         #i = rows = factors
-            for j in range(len(self.result.X[0])):  #j = columns = variables
-                val = self.result.X[i][j]           #from i-row and j-column we had a specific variable value
+        for i in range(len(self.fa_loadings.X)):         #i = rows = factors
+            for j in range(len(self.fa_loadings.X[0])):  #j = columns = variables
+                val = self.fa_loadings.X[i][j]           #from i-row and j-column we had a specific variable value
                 self.insert_item(i + 1, j + 1, val)     #insert into rows and column from 1 onwards, because everything is full of names
 
-    def prazna_funkcija(self): # bc _init_ Slidergrapha requires "callback"
-        pass
-
-    def get_range(self, factor):
-        max_value = factor[0]
-        for i in range(len(factor)):
-            if factor[i] > max_value:
-                max_value = factor[i]
-
-        min_value = factor[0]
-        for i in range(len(factor)):
-            if factor[i] < min_value:
-                min_value = factor[i]
-
-        # adjust and scale by 0.1
-        min_value = min_value - 0.1 * abs(min_value)
-        max_value = max_value + 0.1 * abs(max_value)
-
-        # return the abs value of maximum
-        return max(abs(min_value), abs(max_value))
 
     def set_range(self):
-        factor1_range = self.get_range(self.factor1)
-        factor2_range = self.get_range(self.factor2)
+        factor1_range = np.max(1.1 * np.abs(self.factor1))  #function to remember > the largest abs value * 1.1 of factor
+        factor2_range = np.max(1.1 * np.abs(self.factor2))
         self.plot.setRange(xRange=(-factor1_range, factor1_range), yRange=(-factor2_range, factor2_range))
 
 
@@ -172,30 +158,25 @@ class OWFactorAnalysis(OWWidget):
         if self.n_components == 1:
             return
 
-        self.factor1 = self.result.X[0]
-        self.factor2 = self.result.X[1]
+        self.factor1 = self.fa_loadings.X[0]
+        self.factor2 = self.fa_loadings.X[1]
 
         self.set_range()
 
         foreground = self.plot.palette().text().color()
         foreground.setAlpha(128)
 
-        names = []
-        for i in range(len(self.dataset.domain.attributes)):
-            name = self.dataset.domain.attributes[i].name
-            names.append(name)
+        for x, y, attr in zip(self.factor1, self.factor2, self.attributes):
+            x_vector, y_vector = [0, x], [0, y]
+            self.plot.plot(x_vector, y_vector,
+                pen=mkPen(QColor(Qt.red), width=1), antialias=True,
+            )
 
-        for x, y, n in zip(self.factor1, self.factor2, names):
-            x_vektor, y_vektor = [0, x], [0, y]
-            self.plot.plot(x_vektor, y_vektor, pen=mkPen(QColor(Qt.red) , width=1), antialias=True)
-
-            if n is not None:
-                label = TextItem(
-                    text=n, anchor=(0, 1), color=foreground)
-                label.setPos(x_vektor[-1], y_vektor[-1])
-                self.plot.x = x_vektor
-                self.plot._set_anchor(label, len(x_vektor) - 1, True)
-                self.plot.addItem(label)
+            label = TextItem(text=attr.name, anchor=(0, 1), color=foreground)
+            label.setPos(x_vector[-1], y_vector[-1])
+            self.plot.x = x_vector
+            self.plot._set_anchor(label, len(x_vector) - 1, True)
+            self.plot.addItem(label)
         
         """
         #TABELA TODO: factor loadings po rotaciji 
@@ -216,32 +197,25 @@ class OWFactorAnalysis(OWWidget):
 
     @Inputs.data
     def set_data(self, dataset):
-        #self.closeContext()
-        if dataset is None:
-            self.sample = None
-        else:
-            #self.openContext(dataset.domain)  # what is the function of context?
-            pass
-
         self.dataset = dataset
+        self.attributes = self.dataset.domain.attributes
         self.commit.now()
 
-    def factor_analysis(self):
+    def factor_analysis(self):              #GROMOZANSKI, V OČI BODEČ HROŠČ
         # with chosen n_components and depending on the user-selected rotation, calculate the FA on self.dataset
-        if self.setting_for_rotation == 0:
-            result = FactorAnalysis(self.n_components).fit(self.dataset.X)
-        elif self.setting_for_rotation == 1:
-            result = FactorAnalysis(self.n_components, rotation="varimax").fit(self.dataset.X)
-        elif self.setting_for_rotation == 2:
-            result = FactorAnalysis(self.n_components, rotation="quartimax").fit(self.dataset.X)
+        if self.rotation == Rotation.NoRotation:
+            fa = FactorAnalysis(self.n_components)
+        elif self.rotation == Rotation.Varimax:
+            fa = FactorAnalysis(self.n_components, rotation="varimax")
         else:
-            print("Error:")
+            fa = FactorAnalysis(self.n_components, rotation="quartimax")
+        fa_result = fa.fit(self.dataset.X)
     
         # from result variable (instance of class) only extract the table we are interested in (components)
-        calculated_components = result.components_
+        calculated_components = fa_result.components_
 
         # transform the table back to Orange.data.Table
-        self.result = Table.from_numpy(Domain(self.dataset.domain.attributes), 
+        self.fa_loadings = Table.from_numpy(Domain(self.attributes),
                                       calculated_components)
 
     @gui.deferred
@@ -250,8 +224,8 @@ class OWFactorAnalysis(OWWidget):
             self.Outputs.sample.send(None)
         else:
             self.factor_analysis()
-            # send self.result in Outputs channel
-            self.Outputs.sample.send(self.result)
+            # send self.fa_loadings in Outputs channel
+            self.Outputs.sample.send(self.fa_loadings)
             self.insert_table()
             self.setup_plot()
 
