6944a39f11c871f6530452c7760fc3818e8e1345
suriikata <iris.cilensek1@gmail.com>
2022-06-11 11:14:31 +0200

    stolpična imena

--- factor_analysis/factor_analysis.py
+++ factor_analysis/factor_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from sklearn.decomposition import FactorAnalysis
+from factor_analyzer import FactorAnalyzer
 
 from AnyQt.QtCore import Qt
 from AnyQt.QtGui import QColor, QBrush, QStandardItemModel, QStandardItem
@@ -15,11 +16,7 @@ from orangewidget.utils.widgetpreview import WidgetPreview
 from Orange.widgets.utils.slidergraph import SliderGraph
 from orangewidget import gui
 
-
-
 from pyqtgraph import mkPen, TextItem
-
-
 
 """
 TVORNICA IZBOLJŠAV:
@@ -62,6 +59,7 @@ class OWFactorAnalysis(OWWidget):
         self.dataset = None
         self.attributes = None
         self.fa_loadings = None
+        self.header = []
 
         # Main area settings
         self.attr_box = gui.hBox(self.mainArea, margin=0)
@@ -88,11 +86,12 @@ class OWFactorAnalysis(OWWidget):
         box = gui.vBox(self.mainArea, box = "Factor Loadings")
         
         self.tablemodel = QStandardItemModel(self)
+        self.tablemodel.setHorizontalHeaderLabels(self.header)
         view = self.tableview = QTableView(
             editTriggers=QTableView.NoEditTriggers)
         view.setModel(self.tablemodel)
-        view.horizontalHeader().hide()
-        view.verticalHeader().hide()
+        view.horizontalHeader()
+        view.verticalHeader()
         view.horizontalHeader().setMinimumSectionSize(60)
         #view.selectionModel().selectionChanged.connect(self._invalidate)
         view.setShowGrid(True)
@@ -134,17 +133,15 @@ class OWFactorAnalysis(OWWidget):
     def insert_table(self):
         #insert variable names into first row (0)
         for j in range(len(self.attributes)):
-            name = self.attributes[j].name
-            self.insert_item(0, j + 1, name)        #in order to get the first cell in column > increase j for 1
+            self.header.append(self.attributes[j].name)
 
-        for i in range(self.n_components):
-            name = f"Factor {i + 1}"
-            self.insert_item(i + 1, 0, name)        #in order to get the first cell in row > increase i for 1
+        self.tablemodel.setHorizontalHeaderLabels(self.header)
+        self.header = []
 
         for i in range(len(self.fa_loadings.X)):         #i = rows = factors
             for j in range(len(self.fa_loadings.X[0])):  #j = columns = variables
                 val = self.fa_loadings.X[i][j]           #from i-row and j-column we had a specific variable value
-                self.insert_item(i + 1, j + 1, val)     #insert into rows and column from 1 onwards, because everything is full of names
+                self.insert_item(i, j, val)      #insert into rows and column from 1 onwards, because everything is full of names
 
 
     def set_range(self):
@@ -183,7 +180,7 @@ class OWFactorAnalysis(OWWidget):
         box = gui.vBox(self.mainArea, box = "Factor Loadings")
         #self.left_side.setContentsMargins(0,0,0,0)
         table = self.table_view = QTableView(self.mainArea)
-        #table.setModel(self.table_model) ??? factorsko vnest
+        #table.setModel(self.table_model) ??? faktorsko vnest
         #table.setSelectionMode(QTableView.SingleSelection)
         #table.setSelectionBehavior(QTableView.SelectRows)
         #table.setItemDelegate(gui.ColoredBarItemDelegate(self, color=Qt.cyan))
@@ -201,6 +198,7 @@ class OWFactorAnalysis(OWWidget):
         self.attributes = self.dataset.domain.attributes
         self.commit.now()
 
+
     def factor_analysis(self):              #GROMOZANSKI, V OČI BODEČ HROŠČ
         # with chosen n_components and depending on the user-selected rotation, calculate the FA on self.dataset
         if self.rotation == Rotation.NoRotation:
@@ -209,14 +207,20 @@ class OWFactorAnalysis(OWWidget):
             fa = FactorAnalysis(self.n_components, rotation="varimax")
         else:
             fa = FactorAnalysis(self.n_components, rotation="quartimax")
-        fa_result = fa.fit(self.dataset.X)
+        fa_rotation_result = fa.fit(self.dataset.X)
     
         # from result variable (instance of class) only extract the table we are interested in (components)
-        calculated_components = fa_result.components_
+        calculated_components = fa_rotation_result.components_
 
         # transform the table back to Orange.data.Table
         self.fa_loadings = Table.from_numpy(Domain(self.attributes),
                                       calculated_components)
+
+    def factor_analysis2(self):
+        fa = FactorAnalyzer(rotation = None, n_factors = self.n_components)
+        fa.fit(self.dataset.X)
+        eigen = fa.get_eigenvalues()
+        print(eigen)
 
     @gui.deferred
     def commit(self):
@@ -224,6 +228,7 @@ class OWFactorAnalysis(OWWidget):
             self.Outputs.sample.send(None)
         else:
             self.factor_analysis()
+            self.factor_analysis2()
             # send self.fa_loadings in Outputs channel
             self.Outputs.sample.send(self.fa_loadings)
             self.insert_table()
